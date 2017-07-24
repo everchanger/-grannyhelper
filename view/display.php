f752a5b3dbe491e596fa754db031992ca5785bad
everchanger <joakim.rosenstam@gmail.com>
2017-07-24 23:55:36 +0200

    more fixes
    Fixes to the grid layout, now seperates better.

--- view/display.php
+++ view/display.php
@@ -1,29 +1,26 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="UTF-8">
-		<title>Granny display</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="refresh" content="30" />
+		<title>Granny Display</title>
 		<!-- CSS -->
 		<link rel="stylesheet" href="css/style.css">
 	</head>
 	<body id="display-body">
 
-        <div id="display-date-wrapper">
-            <div class="display-wrapper text-center">
+
+            <div id="display-date-wrapper" class="display-wrapper text-center">
                 <p id="display-date"><?=utf8_encode(ucfirst(strftime("%Aen den %d %B %Y", $date->getTimestamp())));?></p>
             </div>
-        </div>
-        <div id="display-description-wrapper">
-            <div class="display-wrapper alert-border">
+
+            <div id="display-description-wrapper" class="display-wrapper alert-border">
                 <p id="display-title"><?=$event->title?></p>
                 <p id="display-description"><?=nl2br($event->description)?></p>
             </div>
-        </div>
-        <div id="display-img-wrapper">
-            <div class="display-wrapper">
+            <div id="display-img-wrapper" class="display-wrapper text-center">
                 <img id="display-img" src="<?=$event->filename?>">
             </div>
-        </div>
 
             <!-- JAVASCRIPT -->
         <script type="text/javascript" src="js/common.js"></script>
